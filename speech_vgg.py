5a819f2b388d9ff60fa864c2dd424761df49adce
sswwd95 <sswwd95@gmail.com>
2021-04-13 18:01:55 +0900

    commit

--- speech_vgg.py
+++ speech_vgg.py
@@ -2,12 +2,31 @@
 Module containing speechVGG code for network.
 Based on the Keras VGG-16 implementation.
 '''
+# speechVGG는 심층 음성 기능 추출기로, 표현에서 응용 프로그램 및 음성 처리 문제에서 전이 학습을 위해 특별히 맞춤화됩니다. 
+# 추출기는 고전적인 VGG-16 아키텍처를 채택하고 단어 인식 작업을 통해 학습됩니다.
+# 일반화된 음성 표현은 사전 훈련된 모델에 의해 다른 데이터 세트를 사용하여 고유한 음성 처리 작업을 통해 전송할 수 있다
 
 from keras.models import Model
 from keras import layers
 
+# include_top : 네트워크 상단에 3 개의 완전히 연결된 레이어를 포함할지 여부. 
+#               연결하고 더 추가안해서 그냥 기본적인 speech 모델 사용
+# weights : None(무작위 초기화), 'imagenet'(ImageNet 사전 학습) 중 하나
+#           이미지넷의 사전 학습된 가중치이기 때문에 사용안함
+# pooling :include_top이 False일 때 형상 추출을 위한 선택적 풀링 모드입니다. 
+#         - none은 모델의 출력이 마지막 컨볼루션 블록의 4D 텐서 출력이 된다는 것을 의미합니다. 
+#         - avg는 마지막 컨볼루션 블록의 출력에 global average pooling이 적용된다는 것을 의미하므로 모델의 출력이 2D 텐서가 됩니다.
+#         - max는 global max pooling이 적용됩니다.
+# classes : 이미지를 분류 할 선택적 클래스 수, include_top=True 인 경우에만 지정 되고 weights인수가 지정 되지 않은 경우에만 지정됩니다.
+
+# model.trainable = False (전이학습 모델의 특징 추출 부분의 가중치가 고정, 
+#                          고정하면 fit하는 중 지정된 층의 가중치가 업데이트 되지 않는다.)
+# True(기본값)로 하면 전이학습 모델의 특징 추출 부분의 가중치가 흐트러지고 새로 훈련시키는 것
+
+
+
 def speechVGG(include_top=True,
-            weights=None,
+            weights=None, 
             input_shape=(128,128,1),
             classes=8,
             pooling=None,
@@ -82,7 +101,7 @@ def speechVGG(include_top=True,
                       name='block5_conv3')(x)
     x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
-    if include_top:
+    if include_top: # = fc 계층
         # allows to load all layers except these ones that will finetune to task.
         if transfer_learning:
             add_string = '_new'
