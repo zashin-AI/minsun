96ecf2b68ee520f28d31614e25902d7f8391f123
sswwd95 <sswwd95@gmail.com>
2021-04-30 22:22:57 +0900

    commit

--- STT/wav_to_text copy.py
+++ STT/wav_to_text copy.py
@@ -22,46 +22,33 @@ f = open('C:\\nmb\\nmb_data\\STT\\STT voice\\stt_m.txt', 'w')
 
 path_a = 'C:\\nmb\\nmb_data\\STT\\STT voice\\M_denoise\\1_file\\'
 path_b = 'C:\\nmb\\nmb_data\\STT\\STT voice\\M_normal\\1_file\\'
-path_list = list([path_a, path_b])
+path_list = [path_a, path_b]
+
+
+file_list = os.listdir(path_a)
+print(file_list)
 
 for i in range(1,11):
     for j in path_list:
+        infiles = librosa.util.find_files(j)
+        print(infiles)
         if j == path_a:
             a = '[denoise]'
+            print(a)
         elif j == path_b:
             a = '[normal]'
-        k = j[j.rfind('\\') + 1:]
-        print(k)
-#         audio = sr.AudioFile(j + k + '.wav')
-#         with audio as audio_file:
-#             file = r.record(audio_file)
-#         stt = r.recognize_google(file, language='ko-KR')
-#         f.write(a + '\n' + stt + '\n')
-#         print(stt)
-#     f.write('\n')
-# f.close()
-
-# for i in range(1,11):
-#     for j in path_list:
-#         infiles = librosa.util.find_files(j)
-#         print(infiles)
-#         if j == path_a:
-#             a = '[denoise]'
-#             print(a)
-#         elif j == path_b:
-#             a = '[normal]'
-#             print(a)
-#         for infile in infiles:
-#             _, w_id = os.path.split(infile)
-#             w_id = w_id[:-4]
-#             print(w_id)
-#             audio = sr.AudioFile(j + w_id + '.wav')
-#             with audio as audio_file:
-#                 file = r.record(audio_file)
-#             stt = r.recognize_google(file, language='ko-KR')
-#             spelled_sent = spell_checker.check(stt)
-#             checked_sent = spelled_sent.checked
-#             f.write(a + '\n' + checked_sent + '\n')
-#             print(stt)
-#     f.write('\n')
-# f.close()
+            print(a)
+        for infile in infiles:
+            _, w_id = os.path.split(infile)
+            w_id = w_id[:-4]
+            print(w_id)
+            audio = sr.AudioFile(j + w_id + '.wav')
+            with audio as audio_file:
+                file = r.record(audio_file)
+            stt = r.recognize_google(file, language='ko-KR')
+            spelled_sent = spell_checker.check(stt)
+            checked_sent = spelled_sent.checked
+            f.write(a + '\n' + checked_sent + '\n')
+            print(stt)
+    f.write('\n')
+f.close()
