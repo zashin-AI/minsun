b802e428a426d5ce6a3c9a56e0b13e850751a39e
sswwd95 <sswwd95@gmail.com>
2021-05-11 01:11:11 +0900

    commit

--- model/5s_last_model/conv1d_minmax copy.py
+++ model/5s_last_model/conv1d_minmax copy.py
@@ -18,6 +18,7 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 x = np.load('C:\\nmb\\nmb_data\\npy\\5s_last\\total_data.npy') # data
 y = np.load('C:\\nmb\\nmb_data\\npy\\5s_last\\total_label.npy') # label
 
+x = x.reshape(-1, x.shape[1] * x.shape[2])
 
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, shuffle=True, train_size=0.8, random_state=42
@@ -32,6 +33,9 @@ scaler = MinMaxScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
+
+x_train = x_train.reshape(3628, 128, 862)
+x_test = x_test.reshape(908, 128, 862)
 
 # 모델 구성
 model = Sequential()
@@ -69,7 +73,7 @@ model = build_model(x_train.shape[1:], 2)
 
 model.summary()
 
-model.save('C:/nmb/nmb_data/h5/5s_last/model_Conv1D_adam.h5')
+model.save('C:/nmb/nmb_data/h5/5s_last/model_Conv1D_adam_mms.h5')
 
 start = datetime.now()
 
@@ -79,17 +83,18 @@ batch_size = 32
 model.compile(optimizer=op, loss="sparse_categorical_crossentropy", metrics=['acc'])
 es = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True, verbose=1)
 lr = ReduceLROnPlateau(monitor='val_loss', vactor=0.5, patience=10, verbose=1)
-path = 'C:/nmb/nmb_data/h5/5s_last/model_Conv1D_adam.h5'
+path = 'C:/nmb/nmb_data/h5/5s_last/Conv1D_adam_mms.h5'
 mc = ModelCheckpoint(path, monitor='val_loss', verbose=1, save_best_only=True)
 tb = TensorBoard(log_dir='C:/nmb/nmb_data/graph/'+ start.strftime("%Y%m%d-%H%M%S") + "/",histogram_freq=0, write_graph=True, write_images=True)
-history = model.fit(x_train, y_train, epochs=5000, batch_size=batch_size, validation_split=0.2, callbacks=[es, lr, mc, tb])
+# history = model.fit(x_train, y_train, epochs=5000, batch_size=batch_size, validation_split=0.2, callbacks=[es, lr, mc, tb])
 
 
 # 평가, 예측
-model.load_weights('C:/nmb/nmb_data/h5/5s_last/model_Conv1D_adam.h5')
+model.load_weights('C:/nmb/nmb_data/h5/5s_last/Conv1D_adam_mms.h5')
 result = model.evaluate(x_test, y_test, batch_size=batch_size)
-print("loss : ", result[0])
-print("acc : ", result[1])
+print("loss : {:.5f}".format(result[0]))
+print("acc : {:.5f}".format(result[1]))
+
 ############################################ PREDICT ####################################
 pred = ['C:/nmb/nmb_data/predict/5s_last/F','C:/nmb/nmb_data/predict/5s_last/M']
 
@@ -107,19 +112,26 @@ for pred_pathAudio in pred :
         y, sr = librosa.load(file, sr=22050) 
         mels = librosa.feature.melspectrogram(y, sr=sr, hop_length=128, n_fft=512)
         pred_mels = librosa.amplitude_to_db(mels, ref=np.max)
-        pred_mels = pred_mels.reshape(1, pred_mels.shape[0], pred_mels.shape[1])
-        pred_mels = scaler.transform()
-        print(pred_mels.shape)
+        pred_mels = pred_mels.reshape(1, pred_mels.shape[0]*pred_mels.shape[1])
+        # print(pred_mels.shape)
+
+        pred_mels = scaler.transform(pred_mels) # minmaxscaler
+        pred_mels = pred_mels.reshape(1, 128, 862)
+        # print(pred_mels)
 
         y_pred = model.predict(pred_mels)
-        if y_pred == 0:   # 여성이라고 예측
-            print(file, '여자입니다.')
+        # print(y_pred)
+        y_pred_label = np.argmax(y_pred)
+        # print(y_pred_label)
+
+        if y_pred_label == 0:   # 여성이라고 예측
+            print(file[file.rfind('\\') + 1 :], '여자입니다.')
             if name == 'F' :
-                count_f = count_f + 1
+                count_f += 1
         else:                   # 남성이라고 예측
-            print(file, '남자입니다.')
+            print(file[file.rfind('\\') + 1 :], '남자입니다.')
             if name == 'M' :
-                count_m = count_m + 1
+                count_m += 1
                 
                     
 print("43개 여성 목소리 중 "+str(count_f)+"개 정답")
@@ -128,7 +140,7 @@ print("43개 남성 목소리 중 "+str(count_m)+"개 정답")
 
 end = datetime.now()
 time = end - start
-print("time >> " , time)    # time >>  0:00:33.975135
+print("작업 시간 : " , time) 
 
 '''
 __________________________________________________________________________________________________
@@ -238,3 +250,9 @@ Trainable params: 1,767,058
 Non-trainable params: 0
 __________________________________________________________________________________________________
 '''
+
+# loss :  0.07635250687599182
+# acc :  0.9746695756912231
+
+# 43개 여성 목소리 중 42개 정답
+# 43개 남성 목소리 중 39개 정답
