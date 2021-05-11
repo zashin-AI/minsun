54a534cd319e90ac70f8a5b7304defa3b7d84af4
sswwd95 <sswwd95@gmail.com>
2021-05-11 12:15:32 +0900

    commit

--- wav/audio_denoise.py
+++ wav/audio_denoise.py
@@ -5,8 +5,8 @@ sys.path.append('c:/nmb/nada/python_import/')
 from noise_handling import denoise_tim
 
 denoise_tim(
-    load_dir = 'C:\\nmb\\nmb_data\\STT\\STT voice\\M_wav\\M_1_file\\',
-    out_dir = 'C:\\nmb\\nmb_data\\STT\\STT voice\\',
+    load_dir = 'C:\\nmb\\nmb_data\\STT\\slr\\',
+    out_dir = 'C:\\nmb\\nmb_data\\STT\\slr\\denoise\\',
     noise_min = 5000,
     noise_max = 15000,
     n_fft = 512,
