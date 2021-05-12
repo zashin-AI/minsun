9a9203f0729be074dd391aaf75b0308154a1e283
sswwd95 <sswwd95@gmail.com>
2021-05-12 19:32:20 +0900

    commit

--- wav/audio_split.py
+++ wav/audio_split.py
@@ -19,10 +19,10 @@ from voice_handling import voice_split, voice_split_m
 
 
 # for i in range(1, 19):
-filename = 'korea_multi_t12'
+filename = 'korea_multi_t12_0'
 origin_dir = 'C:\\nmb\\nmb_data\\STT\\5s_last_test\\'+ filename + '.wav'
-threshold = 60000 # 몇초씩 자를 것인지 설정
-out_dir = 'C:\\nmb\\nmb_data\\STT\\5s_last_test\\'
+threshold = 5000 # 몇초씩 자를 것인지 설정
+out_dir = 'C:\\nmb\\nmb_data\\STT\\'
 end_threshold = 200000 # 끝나는 지점
 voice_split_m(origin_dir=origin_dir, threshold=threshold, end_threshold =end_threshold,out_dir=out_dir)
 
@@ -39,3 +39,11 @@ voice_split_m(origin_dir=origin_dir, threshold=threshold, end_threshold =end_thr
 #     end_threshold = (60*2)*1000 # 끝나는 지점(1분)
 #     voice_split_1m(origin_dir=origin_dir, threshold=threshold, end_threshold =end_threshold,out_dir=out_dir)
 #     count += 1
+
+0     5000
+5000  10000
+10000 15000
+15000 20000
+20000 25000
+25000 30000
+30000 35000
