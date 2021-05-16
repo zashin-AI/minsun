5eee7d8a882ef92c39c56912ad2ef2da7ed8f3f0
sswwd95 <sswwd95@gmail.com>
2021-05-16 21:31:33 +0900

    commit

--- model/GPT/gpt2.py
+++ model/GPT/gpt2.py
@@ -1,16 +1,5 @@
-https://tech.10000lab.xyz/naturallanguageprocessing/how-to-run-gpt-2.html
-$ git clone https://github.com/openai/gpt-2.git
-$ cd gpt-2
+# https://www.youtube.com/watch?v=3n6157XNYyw
+# 지도학습은 동그란 건 원이다, 네모난 건 사각형이다 라고 학습 데이터를 통해 알려주고 싶은 것을 알려주며 모델을 학습하는 방법인 반면, 
+# zero shot learning은 학습자의 개입이 없이 데이터 있는 그자체로 모델을 학습하여, 하나도 가르쳐주지 않고 학습 된다라는 의미를 가지고 있습니다.
 
-https://tech.10000lab.xyz/naturallanguageprocessing/how-to-run-gpt-2.html
-
-
-$ pip install tensorflow==1.15
-$ pip install -r requirements.txt
-
-$ python download_model.py 124M
-$ python download_model.py 355M
-$ python download_model.py 774M
-$ python download_model.py 1558M
-
-python src/interactive_conditional_samples.py --model_name="355M"
+# https://blog.naver.com/PostView.nhn?blogId=horajjan&logNo=222104684132&parentCategoryNo=&categoryNo=120&viewDate=&isShowPopularPosts=true&from=search
