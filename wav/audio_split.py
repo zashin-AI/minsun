d92b01287b7d6ccecd8a62aec1cb9140c7710b22
sswwd95 <sswwd95@gmail.com>
2021-06-02 15:14:05 +0900

    commit

--- wav/audio_split.py
+++ wav/audio_split.py
@@ -17,33 +17,33 @@ from voice_handling import voice_split, voice_split_m
 
 
 
-
+'''
 # for i in range(1, 19):
 filename = 'korea_multi_t12_0'
 origin_dir = 'C:\\nmb\\nmb_data\\STT\\5s_last_test\\'+ filename + '.wav'
-threshold = 5000 # 몇초씩 자를 것인지 설정
+threshold = 10000 # 몇초씩 자를 것인지 설정
 out_dir = 'C:\\nmb\\nmb_data\\STT\\'
 end_threshold = 200000 # 끝나는 지점
 voice_split_m(origin_dir=origin_dir, threshold=threshold, end_threshold =end_threshold,out_dir=out_dir)
-
+'''
 
 # 여러 파일 for 문 사용
-# audio_dir = 'C:\\nmb\\nmb_data\\audio\\open_slr_wav\\'
-# infiles = librosa.util.find_files(audio_dir)
+audio_dir = 'C:\\nmb\\nmb_data\\10s2m\\open_slr_f_silence_sum\\'
+infiles = librosa.util.find_files(audio_dir)
 
-# count = 0
-# for file in infiles:
-#     origin_dir = infiles[count]
-#     threshold = 5000 # 몇초씩 자를 것인지 설정
-#     out_dir = 'C:\\nmb\\nmb_data\\audio\\open_slr_f_2m\\'
-#     end_threshold = (60*2)*1000 # 끝나는 지점(1분)
-#     voice_split_1m(origin_dir=origin_dir, threshold=threshold, end_threshold =end_threshold,out_dir=out_dir)
-#     count += 1
+count = 0
+for file in infiles:
+    origin_dir = infiles[count]
+    threshold = 10000 # 몇초씩 자를 것인지 설정
+    out_dir = 'C:\\nmb\\nmb_data\\audio_data\\10s2m\\f\\'
+    end_threshold = (60*2)*1000 # 끝나는 지점
+    voice_split_m(origin_dir=origin_dir, threshold=threshold, end_threshold =end_threshold,out_dir=out_dir)
+    count += 1
 
-0     5000
-5000  10000
-10000 15000
-15000 20000
-20000 25000
-25000 30000
-30000 35000
+# 0     5000
+# 5000  10000
+# 10000 15000
+# 15000 20000
+# 20000 25000
+# 25000 30000
+# 30000 35000
